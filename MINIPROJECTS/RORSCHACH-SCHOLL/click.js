69137e83680a7982e8f2a1b2474e3e4d1321d7a9
apademide <contact@apademide.ch>
2023-04-14 13:31:51 +0200

    imp: style svg on hoer/selected

--- MINIPROJECTS/RORSCHACH-SCHOLL/click.js
+++ MINIPROJECTS/RORSCHACH-SCHOLL/click.js
@@ -4,6 +4,11 @@
   const buttons = document.querySelectorAll("[data-color]");
   for (const button of buttons) {
     button.addEventListener("click", function () {
+      for (const button of buttons) {
+        button.classList.remove("current");
+      }
+      this.classList.add("current");
+
       body.dataset.color = this.dataset.color;
     });
   }
