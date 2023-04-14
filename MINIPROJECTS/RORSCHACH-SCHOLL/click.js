d43063c6cc265f5505c3d284e14d36c42e284868
apademide <contact@apademide.ch>
2023-04-14 13:36:04 +0200

    feat: clicking buttons change svg

--- MINIPROJECTS/RORSCHACH-SCHOLL/click.js
+++ MINIPROJECTS/RORSCHACH-SCHOLL/click.js
@@ -2,7 +2,15 @@
   const body = document.body;
 
   const buttons = document.querySelectorAll("[data-color]");
+
+  const uses = [
+    document.getElementById("shape-main").querySelector("use"),
+    document.getElementById("shape-mirror").querySelector("use"),
+  ];
+
   for (const button of buttons) {
+    const xlink = button.querySelector("use").getAttribute("xlink:href");
+
     button.addEventListener("click", function () {
       for (const button of buttons) {
         button.classList.remove("current");
@@ -10,6 +18,10 @@
       this.classList.add("current");
 
       body.dataset.color = this.dataset.color;
+
+      for (const use of uses) {
+        use.setAttribute("xlink:href", xlink);
+      }
     });
   }
 })();
