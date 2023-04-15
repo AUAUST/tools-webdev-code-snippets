17fc8ae41f80b6d40be065813e1ac0b5e54b52da
apademide <contact@apademide.ch>
2023-04-15 11:41:28 +0200

    feat: separate colors from icons

--- MINIPROJECTS/RORSCHACH-SCHOLL/click.js
+++ MINIPROJECTS/RORSCHACH-SCHOLL/click.js
@@ -1,16 +1,11 @@
 (function () {
+  // For colors buttons
+
   const body = document.body;
 
   const buttons = document.querySelectorAll("[data-color]");
 
-  const svgUses = [
-    document.getElementById("shape-main").querySelector("use"),
-    document.getElementById("shape-mirror").querySelector("use"),
-  ];
-
   for (const button of buttons) {
-    const xlink = button.querySelector("use").getAttribute("xlink:href");
-
     button.addEventListener("click", function () {
       for (const button of buttons) {
         button.classList.remove("current");
@@ -18,10 +13,25 @@
       this.classList.add("current");
 
       body.dataset.color = this.dataset.color;
+    });
+  }
+})();
 
-      for (const svgUse of svgUses) {
-        svgUse.setAttribute("xlink:href", xlink);
+(function () {
+  // For shapes (icons) buttons
+
+  const body = document.body;
+
+  const buttons = document.querySelectorAll("[data-icon]");
+
+  for (const button of buttons) {
+    button.addEventListener("click", function () {
+      for (const button of buttons) {
+        button.classList.remove("current");
       }
+      this.classList.add("current");
+
+      body.dataset.icon = this.dataset.icon;
     });
   }
 })();
