5e6c08f1bc9c97c4e5faace2e0a589761f5bb04a
apademide <contact@apademide.ch>
2023-05-31 22:38:00 +0200

    feat: switch slides on scroll

--- MINIPROJECTS/RORSCHACH-SCHOLL/js/introduction.js
+++ MINIPROJECTS/RORSCHACH-SCHOLL/js/introduction.js
@@ -10,5 +10,48 @@
     (a, b) => a.dataset.index - b.dataset.index
   );
 
+  const state = {
+    // Config
+    scrollAmount: 50,
+    animationLength: (function () {
+      const durationString = getComputedStyle(slides[0]).transitionDuration;
+      return parseFloat(durationString) * 1000;
+    })(),
+    lastSlide: slides[slides.length - 1],
+    // State
+    currentSlide: 0,
+    animating: false,
+    scrollPosition: 0,
+    finished: false,
+  };
+
+  window.addEventListener("wheel", (e) => {
+    if (state.animating) return;
+
+    state.scrollPosition += e.deltaY;
+
+    const newSlide = Math.floor(state.scrollPosition / state.scrollAmount);
+    console.log(state.scrollPosition, newSlide);
+
+    if (newSlide >= slides.length) {
+      state.finished = true;
+    } else if (newSlide !== state.currentSlide) {
+      // Prevent scrolling past multiple slides
+      state.animating = true;
+      setTimeout(() => {
+        state.animating = false;
+      }, state.animationLength);
+
+      // Show new slide
+      for (const slide of slides) {
+        slide.classList.remove("current");
+      }
+      slides[newSlide].classList.add("current");
+
+      // Update state
+      state.currentSlide = newSlide;
+    }
+  });
+
   slides[0].classList.add("current");
 })();
