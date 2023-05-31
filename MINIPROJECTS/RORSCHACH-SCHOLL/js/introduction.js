ab5f65c3d4c40f218cf3b0d5e0a5c5d77000456b
apademide <contact@apademide.ch>
2023-05-31 22:27:03 +0200

    feat: scroll tooltip

--- MINIPROJECTS/RORSCHACH-SCHOLL/js/introduction.js
+++ MINIPROJECTS/RORSCHACH-SCHOLL/js/introduction.js
@@ -1,7 +1,14 @@
 (function () {
+  // Scroll tooltip
+  const scrollMe = document.getElementById("scroll-me");
+  setTimeout(() => {
+    scrollMe.classList.add("current");
+  }, 1000);
+
   const introduction = document.getElementById("introduction");
   const slides = Array.from(introduction.querySelectorAll(".slide")).sort(
     (a, b) => a.dataset.index - b.dataset.index
   );
+
   slides[0].classList.add("current");
 })();
